426256a168224816a4893833041b7a1634c97aaa
Avishrantsh <avishrants@gmail.com>
2021-05-26 19:35:17 +0530

    Enhancement: CreateClassifier changes, performance improvements

--- main.go
+++ main.go
@@ -1,3 +1,10 @@
+/*
+	Error Codes:
+	E1 - Failure in loading licenses from given directory.
+	E2 - File Not Found at specified path.
+	E3 - No matching license found.
+*/
+
 package main
 
 import "C"
@@ -9,10 +16,13 @@ import (
 	"github.com/avishrantssh/GoLicenseClassifier/classifier"
 )
 
+// Default Threshold for Filtering the results
 var defaultThreshold = 0.8
+
+// Base Licenses Root Directory
 var baseLicenses = "./classifier/licenses"
 
-// CreateClassifier creates a classifier instance
+// Create a classifier instance and load base licenses
 func CreateClassifier() (*classifier.Classifier, error) {
 	c := classifier.NewClassifier(defaultThreshold)
 	return c, c.LoadLicenses(baseLicenses)
@@ -22,23 +32,28 @@ func CreateClassifier() (*classifier.Classifier, error) {
 func FindMatch(filepath *C.char) *C.char {
 	var status []string
 	patharr := GetPaths(C.GoString(filepath))
+
+	// A simple channel implementation to lock function until execution is complete
 	sem := make(chan struct{})
+	c, err := CreateClassifier()
+
+	// fmt.Println("Finished Reading licenses")
+	if err != nil {
+		return C.CString("E1")
+	}
+
 	for _, path := range patharr {
+
 		// Go Routine far faster Processing
 		go func(path string) {
 			b, err := ioutil.ReadFile(path)
 			// File Not Found
 			if err != nil {
-				status = append(status, "E1,"+path)
+				status = append(status, "E2,"+path)
 			}
 
 			data := []byte(string(b))
-
-			c, err := CreateClassifier()
 			// Internal Error in Initializing Classifier
-			if err != nil {
-				status = append(status, "E2,"+err.Error())
-			}
 
 			m := c.Match(data)
 			var tmp string
@@ -52,6 +67,7 @@ func FindMatch(filepath *C.char) *C.char {
 			} else {
 				status = append(status, path+":"+tmp)
 			}
+
 			sem <- struct{}{}
 		}(path)
 	}
