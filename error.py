c456b107c300ee28b52528199253d896b84166bf
Avishrantsh <avishrants@gmail.com>
2021-05-31 11:35:55 +0530

    Clean up ignored files

--- error.py
+++
@@ -1,4 +0,0 @@
-class PathIsDir(Exception):
-    """Exception raised when given path does not correspond to file"""
-    def __str__(self):
-        return "The given path does not correspond to a file"
